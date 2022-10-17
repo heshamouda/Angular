2bcaf13eccb1399885a78964182ee5aa72f4eaa9
Hesham Ouda <hesham.ouda@students.fhnw.ch>
2022-10-17 21:26:47 +0200

    delete and create new project

--- demoApp/src/app/app.component.spec.ts
+++ demoApp/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'myFirstApp'`, () => {
+  it(`should have as title 'demoApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('myFirstApp');
+    expect(app.title).toEqual('demoApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('myFirstApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demoApp app is running!');
   });
 });
