b6a2cb24dda68b4e597bc1b369f1c4adb3732324
heshamouda <hesham.ouda@outlook.com>
2022-10-19 12:12:18 +0200

    create 2 components to give an alert messge

--- demoApp/src/app/app.module.ts
+++ demoApp/src/app/app.module.ts
@@ -4,13 +4,17 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent,
+    ServerComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent
   ],
   imports: [
     BrowserModule,
